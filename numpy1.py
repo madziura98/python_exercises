73cb3cfe1d3fde6f8782c386179dfda0c3901db4
madziura98 <34924237+madziura98@users.noreply.github.com>
2018-08-27 10:27:02 +0200

    Update numpy1.py

--- numpy1.py
+++ numpy1.py
@@ -51,6 +51,22 @@ array([[1, 1, 1],
 >>> np.hstack([p, 2*p]) #horizontal
 array([[1, 1, 1, 2, 2, 2],
        [1, 1, 1, 2, 2, 2]])
+>> #concatenating arrays
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+np.concatenate([a,b])
+
+# vertically stack the arrays
+ np.vstack([x, grid])
+Out[48]: array([[1, 2, 3],
+ [9, 8, 7],
+ [6, 5, 4]])
+In[49]: # horizontally stack the arrays
+ y = np.array([[99],
+ [99]])
+ np.hstack([grid, y])
+Out[49]: array([[ 9, 8, 7, 99],
+ [ 6, 5, 4, 99]])
        
 a.argmin()
 a.argmax()
@@ -67,3 +83,17 @@ r_copy[:] = 10
 
 test = np.random.randint(0, 10, (4,3))
 test
+
++ np.add Addition (e.g., 1 + 1 = 2)
+- np.subtract Subtraction (e.g., 3 - 2 = 1)
+- np.negative Unary negation (e.g., -2)
+* np.multiply Multiplication (e.g., 2 * 3 = 6)
+/ np.divide Division (e.g., 3 / 2 = 1.5)
+// np.floor_divide Floor division (e.g., 3 // 2 = 1)
+** np.power Exponentiation (e.g., 2 ** 3 = 8)
+% np.mod Modulus/remainder (e.g., 9 % 4 = 1)
+np.abs() #zwraca wartość bezwzględną liczby
+
+x = np.arange(1,6)
+np.add.reduce(x) #output 15
+np.add.accumulate()
